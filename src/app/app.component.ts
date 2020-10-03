9d52be5204e173f4afb00e58d391cb32e83423e6
pjaro <pjarosz27@wp.pl>
2020-10-03 13:53:18 +0200

    Zad 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tab = 0;
+export class AppComponent implements OnInit {
+  borderStyle = '1px solid black';
+  alertStyle = {
+    color: 'red',
+    fontWeight: 'bold',
+    borderBottom: this.borderStyle
+  };
 
-  setTab(num: number): void{
-    this.tab = num;
+  style: any;
+
+  setStyle(style: any): void {
+    this.style = style;
   }
 
-  isSelected(num: number): boolean {
-    return this.tab === num;
+  ngOnInit(): void {
+    this.setStyle(this.alertStyle);
   }
 }
