b512adb19e398a0b10c94aae4c622f015e183540
pjaro <pjarosz27@wp.pl>
2020-10-04 10:03:05 +0200

    Zad 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,16 +7,22 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  @ViewChild('tdForm')
-  tdForm: any;
+
+  modelForm: any;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    this.modelForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      age: new FormControl(),
+      country: new FormControl()
+    });
   }
 
   onSubmit(): void {
-    console.log(this.tdForm.value);
+    console.log(this.modelForm.value);
   }
 }
